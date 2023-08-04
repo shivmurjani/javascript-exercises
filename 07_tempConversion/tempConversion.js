f59850448f03d10db167a0f5a2319d0ca590a92f
Shivansh Murjani <murjani.vmware@gmail.com>
2023-08-04 08:06:49 +0530

    1-7 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+  let converted = (number-32)*5/9;
+  
+  let c = converted.toFixed(1);
+  return Math.round(converted*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+  let converted = (number*9/5)+32;
+   
+   return Math.round(converted*10)/10;
 };
 
 // Do not edit below this line
